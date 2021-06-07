e70e7886d56a2c3ec5f398fd9bcb07f10f9eb55d
jaystyba <jaystyba@gmail.com>
2021-06-06 21:59:53 -0500

    Salon Rank Fix

--- app/src/pages/reports/SalonRankings.js
+++ app/src/pages/reports/SalonRankings.js
@@ -2,7 +2,7 @@ import React from "react";
 import { QueryReport } from "../../components/QueryReport";
 
 const EmployeeRankings = () => {
-  return <QueryReport title={"Salon Rankings"} queryPath={"salon-rankings"} />;
+  return <QueryReport title={"Salon Rank"} queryPath={"salon-rankings"} />;
 };
 
 export default EmployeeRankings;
